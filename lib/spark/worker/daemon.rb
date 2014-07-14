1a4ff8ef75ab9b9b09c001472b58174e6eec41dc
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2014-07-14 14:56:31 +0200

    log

--- lib/spark/worker/daemon.rb
+++ lib/spark/worker/daemon.rb
@@ -1,9 +1,17 @@
 #!/usr/bin/env ruby
 
+# $stderr.reopen("/ruby_spark/err.txt", "w")
+
 require "socket"
 
 def log(message=nil)
   puts %{==> [#{Process.pid}] [#{Time.now.strftime("%H:%M")}] RUBY WORKER: #{message}}
+end
+
+def realtime
+  t1 = Time.now
+  yield
+  Time.now - t1
 end
 
 # ==============================================================================
@@ -95,6 +103,97 @@ end
 # Worker
 # ==============================================================================
 
+# class Worker
+
+#   include SocketHelper
+
+#   attr_accessor :client_socket
+
+#   def initialize(client_socket)
+#     self.client_socket = client_socket
+#   end
+
+#   def run
+#     time = Time.now
+
+#     split_index = read_int
+
+#     command = Marshal.load(read(read_int))
+
+#     log "1: #{-1*(time - (time=Time.now))*1000}ms"
+#     iterator = load_iterator
+
+#     log "2: #{-1*(time - (time=Time.now))*1000}ms"
+#     eval(command[0]) # original lambda
+
+#     log "3: #{-1*(time - (time=Time.now))*1000}ms"
+#     result = eval(command[1]).call(split_index, iterator)
+#     log "4: #{-1*(time - (time=Time.now))*1000}ms"
+    
+#     write_stream(result)
+#     log "5: #{-1*(time - (time=Time.now))*1000}ms"
+
+#     write_int(0)
+#   end
+
+#   private
+
+#     def read(size)
+#       client_socket.read(size)
+#     end
+
+#     def send(data)
+#       client_socket.send(data, 0)
+#     end
+
+#     def read_int
+#       read(4).unpack("l>")[0] 
+#     end
+
+#     def write_int(data)
+#       send(to_stream(data))
+#     end
+
+#     def load_iterator(&block)
+#       # result = []
+#       # loop { 
+#       #   result << begin
+#       #               data = read(read_int).force_encoding(@encoding) rescue break # end of stream
+#       #               Marshal.load(data) rescue data # data cannot be mashaled (e.g. first input)
+#       #             end
+#       # }
+#       # result
+
+#       Enumerator.new do |e|
+#         while true
+#           begin
+#             e.yield(read(read_int))
+#           rescue
+#             break
+#           end
+#         end
+#       end.each(&block)
+
+#     end
+
+#     def write_stream(data)
+#       data.each do |x|
+#         serialized = Marshal.dump(x)
+
+#         write_int(serialized.size)
+#         send(serialized)
+#       end
+#     end
+
+# end
+
+
+
+
+
+
+
+
 class Worker
 
   include SocketHelper
@@ -103,46 +202,51 @@ class Worker
 
   def initialize(client_socket)
     self.client_socket = client_socket
+
+    @iterator = []
+    @point = Time.now
   end
 
   def run
-    time = Time.now.to_i
 
     split_index = read_int
-    command_size = read_int
 
-    log "1: #{-1*(time - (time=Time.now.to_i))}s"
+    command = Marshal.load(read(read_int))
 
-    command = Marshal.load(read(command_size))
+    report("Load command")
 
-    log "2: #{-1*(time - (time=Time.now.to_i))}s"
+    load_iterator
 
-    iterator = load_iterator
-
-    log "3: #{-1*(time - (time=Time.now.to_i))}s"
+    report("Load iterator")
 
     eval(command[0]) # original lambda
 
-    log "4: #{-1*(time - (time=Time.now.to_i))}s"
+    report("Eval original lambda")
 
-    result = eval(command[1]).call(split_index, iterator)
+    @result = eval(command[1]).call(split_index, @iterator)
 
-    log "5: #{-1*(time - (time=Time.now.to_i))}s"
+    report("Run")
+    
+    write_stream
 
-    write_stream(result)
+    report("Write stream")
+
     write_int(0)
-
-    log "6: #{-1*(time - (time=Time.now.to_i))}s"
   end
 
   private
+
+    def report(message)
+      log("#{message}: #{(Time.now - @point)*1000}ms")
+      @point = Time.now
+    end
 
     def read(size)
       client_socket.read(size)
     end
 
     def send(data)
-      client_socket.send(data, 0)
+      client_socket.write(data)
     end
 
     def read_int
@@ -153,38 +257,44 @@ class Worker
       send(to_stream(data))
     end
 
-    def load_iterator(&block)
-      # result = []
-      # loop { 
-      #   result << begin
-      #               data = read(read_int).force_encoding(@encoding) rescue break # end of stream
-      #               Marshal.load(data) rescue data # data cannot be mashaled (e.g. first input)
-      #             end
-      # }
-      # result
+    def load_iterator
 
-      Enumerator.new do |e|
-        while true
-          begin
-            e.yield(read(read_int))
-          rescue
-            break
-          end
-        end
-      end.each(&block)
+      @iterator = []
+      loop { 
+        @iterator << begin
+                       # data = read(read_int).force_encoding(@encoding) rescue break # end of stream
+                       data = read(read_int) rescue break # end of stream
+                       # Marshal.load(data) rescue data # data cannot be mashaled (e.g. first input)
+                     end
+      }
+
+      # Enumerator.new do |e|
+      #   while true
+      #     begin
+      #       e.yield(read(read_int))
+      #     rescue
+      #       break
+      #     end
+      #   end
+      # end.each(&block)
 
     end
 
-    def write_stream(data)
-      data.each do |x|
+    def write_stream
+      @result.each{|x|
         serialized = Marshal.dump(x)
 
         write_int(serialized.size)
         send(serialized)
-      end
+      }
     end
 
 end
+
+
+
+
+
 
 
 
