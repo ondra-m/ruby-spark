b07d54a8e1e7d066335c27294138fcec4cd50bb3
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2014-09-23 22:07:34 +0200

    new method for sorting (faster)

--- benchmark/sort2.rb
+++ benchmark/sort2.rb
@@ -1,10 +1,14 @@
 require "benchmark"
 require "algorithms"
 
-NUMBER_OF_SORTING = 10
-NUMBER_OF_ARRAY   = 20
-WORDS_IN_ARRAY    = 10000
+NUMBER_OF_SORTING = 1
+NUMBER_OF_ARRAY   = 5
+WORDS_IN_ARRAY    = 500000
 MAX_WORD_SIZE     = 10
+
+# Rozdíl mezi 1 a 10 je 2x
+# ale mezi 10 a 50 žádný
+EVAL_N_VALUES     = 10 # sort1_2
 
 def words
   Array.new(WORDS_IN_ARRAY) { word }
@@ -49,7 +53,49 @@ def sort1(data)
   end
 end
 
-# sorted_array = sort1(array)
+
+# =================================================================================================
+# Sort1_2
+
+# Vrátí nový (nevyhodnocený) enumerator
+def sort1_2(data)
+  return to_enum(__callee__, data) unless block_given?
+
+  heap = []
+  enums = []
+
+  # Inicializuji heap s prvními položkami
+  # připojím samotné enumeratory pro volání .next
+  data.each do |a|
+    EVAL_N_VALUES.times {
+      heap << [a.next, a]
+    }
+  end
+
+  while data.any? || heap.any?
+      # Seřadím pole podle hodnot
+      heap.sort_by!{|(item,_)| item}
+
+      # Minimálně můžu vzít EVAL_N_VALUES
+      EVAL_N_VALUES.times {
+        # Uložím si hodnotu a enumerator
+        item, enum = heap.shift
+        # Hodnota půjde do výsledku
+        yield item
+
+        enums << enum
+      }
+
+    while (enum = enums.shift)
+      begin
+        heap << [enum.next, enum]
+      rescue StopIteration
+        data.delete(enum)
+      end
+    end
+
+  end
+end
 
 
 # =================================================================================================
@@ -78,8 +124,6 @@ def sort2(data)
   end
 end
 
-# sorted_array = sort2(array)
-
 
 # =================================================================================================
 # Benchmark
@@ -94,6 +138,12 @@ Benchmark.bm(10) do |x|
   x.report("sort 1") do
     NUMBER_OF_SORTING.times { 
       raise "Bad sorting" if @result != sort1(@array.map(&:each)).to_a
+    }
+  end
+
+  x.report("sort 1_2") do
+    NUMBER_OF_SORTING.times { 
+      raise "Bad sorting" if @result != sort1_2(@array.map(&:each)).to_a
     }
   end
 
