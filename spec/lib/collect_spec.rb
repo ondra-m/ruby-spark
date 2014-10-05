0d70d80341c22ce82b2451c7f2f18793dfff08fb
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2014-10-05 16:09:22 +0200

    take spec

---
+++ spec/lib/collect_spec.rb
@@ -0,0 +1,42 @@
+require "spec_helper"
+
+RSpec::describe Spark::RDD do
+
+  let(:mapping) { lambda{|x| [x, 1]} }
+  let(:numbers) { Generator.numbers }
+  
+  it ".collect_as_hash" do
+    rdd = $sc.parallelize(numbers)
+    rdd = rdd.map(mapping)
+
+    expect(rdd.collect_as_hash).to eql(Hash[numbers.map(&mapping)])
+  end
+
+  context ".take" do
+    let(:size)    { 1000 }
+    let(:numbers) { Generator.numbers(size) }
+    let(:rdd)     { $sc.parallelize(numbers) }
+
+    it "nothing" do
+      expect(rdd.take(0)).to eql([])
+    end
+
+    it "first" do
+      expect(rdd.first).to eql(numbers.first)
+    end
+
+    it "less than limit" do
+      _size = size / 2
+      expect(rdd.take(_size)).to eql(numbers.take(_size))
+    end
+
+    it "all" do
+      expect(rdd.take(size)).to eql(numbers)
+    end
+
+    it "more than limit" do
+      expect(rdd.take(size*2)).to eql(numbers)
+    end
+  end
+
+end
