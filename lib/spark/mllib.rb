71ce01f57a606193061f5faab12cbd244accbd4c
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2015-02-22 21:39:38 +0100

    mllib load

--- lib/spark/mllib.rb
+++ lib/spark/mllib.rb
@@ -4,9 +4,15 @@ module Spark
     autoload :DenseVector,  'spark/mllib/vector'
     autoload :SparseVector, 'spark/mllib/vector'
 
+    # Regression
+    autoload :LabeledPoint,            'spark/mllib/regression'
+    autoload :LinearRegressionWithSGD, 'spark/mllib/regression'
+
     def self.load
       Object.const_set(:DenseVector, DenseVector)
       Object.const_set(:SparseVector, SparseVector)
+      Object.const_set(:LabeledPoint, LabeledPoint)
+      Object.const_set(:LinearRegressionWithSGD, LinearRegressionWithSGD)
       nil
     end
   end
