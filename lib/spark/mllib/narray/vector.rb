ab52a9f56b0dd34ab199c26c9c1628aa3d10e391
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2015-02-23 17:22:23 +0100

    fix narray initialize

--- lib/spark/mllib/narray/vector.rb
+++ lib/spark/mllib/narray/vector.rb
@@ -2,32 +2,41 @@ require 'narray'
 
 module Spark
   module Mllib
-    class BaseVector < ::NArray
+    class BaseVector
 
       def initialize(stype, *args)
-binding.pry unless $__binding
         case stype.to_s.downcase
-
         when 'dense'
-          values = args.shift
-          dtype = args.shift
-          super(dtype, values.size)
-          self[] = values
-
+          init_as_dense(*args)
         when 'sparse'
-          size = args.shift
-          values = args.shift
-          dtype = args.shift
-          super(dtype, size)
-          fill!(0)
-
+          init_as_sparse(*args)
         else
           raise ArgumentError, 'Vector must be Dense or Sparse.'
         end
       end
 
+      def init_as_dense(*args)
+        values = args.shift
+        dtype = args.shift
+        @__vector__ = NArray.new(dtype, values.size)
+        self[] = values
+      end
+
+      def init_as_sparse(*args)
+        size = args.shift
+        values = args.shift
+        dtype = args.shift
+        @__vector__ = NArray.new(dtype, size)
+        fill!(0)
+      end
+
       def values
         to_a
+      end
+
+      # NArray is initialized on `self.new` instead of `initialize`
+      def method_missing(method, *args, &block)
+        @__vector__.__send__(method, *args, &block)
       end
 
     end
