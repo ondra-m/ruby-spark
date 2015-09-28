657f03994f11cf9eeff2764d9f638befd48facbf
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2015-09-28 19:02:41 +0200

    sql column operator

--- lib/spark/sql/column.rb
+++ lib/spark/sql/column.rb
@@ -16,12 +16,76 @@ module Spark
         @jcolumn = jcolumn
       end
 
+      def !
+        func_op('negate')
+      end
+
+      def ~
+        func_op('not')
+      end
+
       def +(item)
         bin_op('plus', item)
       end
 
+      def -(item)
+        bin_op('minus', item)
+      end
 
+      def *(item)
+        bin_op('multiply', item)
+      end
 
+      def /(item)
+        bin_op('divide', item)
+      end
+
+      def %(item)
+        bin_op('mod', item)
+      end
+
+      def ==(item)
+        bin_op('equalTo', item)
+      end
+
+      def !=(item)
+        bin_op('notEqual', item)
+      end
+
+      def <(item)
+        bin_op('lt', item)
+      end
+
+      def <=(item)
+        bin_op('leq', item)
+      end
+
+      def >(item)
+        bin_op('gt', item)
+      end
+
+      def >=(item)
+        bin_op('geq', item)
+      end
+
+      def &(item)
+        bin_op('and', item)
+      end
+
+      def |(item)
+        bin_op('or', item)
+      end
+
+      # Returns this column aliased with a new name or names (in the case of expressions that
+      # return more than one column, such as explode).
+      #
+      # == Example:
+      #   df.select(df.age.alias("age2")).collec
+      #   # => [#<Row {"age2"=>2}>, #<Row {"age2"=>5}>]
+      #
+      def alias(name)
+        Column.new(jcolumn.as(name))
+      end
 
       def bin_op(name, item)
         if item.is_a?(Column)
@@ -31,6 +95,11 @@ module Spark
         end
 
         new_jcolumn = jcolumn.__send__(name, col)
+        Column.new(new_jcolumn)
+      end
+
+      def func_op(name)
+        new_jcolumn = JSQLFunctions.__send__(name, jcolumn)
         Column.new(new_jcolumn)
       end
 
